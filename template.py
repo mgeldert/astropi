400d5db8546cdf631f5b81695ccbb695fa8f4814
Matthew Geldert <mgeldert@pulsesecure.net>
2019-12-03 13:26:06 +0000

    Initial commit

---
+++ template.py
@@ -0,0 +1,27 @@
+from sense_hat import SenseHat
+from time import sleep
+sense = SenseHat()
+sense.set_rotation(270)
+
+b = (  0,   0,   0)   # Black
+w = (255, 255, 255)   # White
+r = (255,   0,   0)   # Red
+o = (255, 127,   0)   # Orange
+y = (255, 255,   0)   # Yellow
+g = (  0, 255,   0)   # Green
+B = (  0,   0, 255)   # Blue
+i = ( 75,   0, 130)   # Indigo
+v = (148,   0, 211)   # Violet
+
+
+picture = [
+  w, r, o, y, g, B, i, v,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b
+]
+
